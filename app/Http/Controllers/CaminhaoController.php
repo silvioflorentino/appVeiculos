ad02f8e78ea5a946555bff9466b8342d55d263c0
SILVIO WANDERSON FLORENTINO <silvio.wflorentino@senacsp.edu.br>
2022-10-13 17:10:50 -0300

    Enviar para o banco de dados

---
+++ app/Http/Controllers/CaminhaoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+use App\Models\Caminhao;
+
+class CaminhaoController extends Controller
+{
+    public function FormularioCadastro(){
+        return view('cadastrarCaminhao');
+    }
+
+    public function Editar(){
+        return view('editarCaminhao');
+    }
+
+    public function SalvarBanco(Request $request){
+
+        $dadosCaminhao = $request->validate([
+            'modelo' => 'string|required',
+            'marca' => 'string|required',
+            'ano' =>'string|required',
+            'cor' => 'string|required',
+            'valor' => 'string|nullable'
+        ]);
+
+        Caminhao::create($dadosCaminhao);
+        return Redirect::route('home');
+    }
+
+
+}
---
+++ app/Models/Caminhao.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Caminhao extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'modelo','marca','ano','cor','valor'
+    ];
+}
